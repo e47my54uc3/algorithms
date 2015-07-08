91b5034613709dcf3c7d28c82945e90e49220f11
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-07 18:01:33 -0700

    Add doublylinkedlist skeleton

---
+++ java_files/DoublyLinkedListDemo.java
@@ -0,0 +1,13 @@
+public class DoublyLinkedListDemo {
+  public static void main(String[] args){
+    DoublyLinkedList dList = new DoublyLinkedList();
+
+      dList.insertAtTail(9);
+
+      dList.insertAtTail(10);
+      dList.insertAtTail(11);
+
+      System.out.println(dList);
+    
+  }
+}
